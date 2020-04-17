4b618da8062b9dc410f4a02abdd47ba3d8d5d8a0
Julian Gonzalez <jgonz156@lion.lmu.edu>
2020-04-16 21:16:09 -0700

    Fixed bug

--- Sort by Character Frequency.js
+++ Sort by Character Frequency.js
@@ -14,37 +14,38 @@ var frequencySort = function (s) {
     let frequency = new Array(arrayOfString.length);
     let database = [characters, frequency];
 
-    for (i = 0; i < arrayOfString.length; i++) {
-        if (arrayOfString[i] === characters[characters.indexOf(arrayOfString[i])]) {
-            frequency[characters.indexOf(arrayOfString[i])]++;
+    for (c = 0; c < arrayOfString.length; c++) {
+        if (arrayOfString[c] === characters[characters.indexOf(arrayOfString[c])]) {
+            frequency[characters.indexOf(arrayOfString[c])]++;
         } else {
-            characters[i] = arrayOfString[i];
-            frequency[i] = 1;
+            characters[c] = arrayOfString[c];
+            frequency[c] = 1;
         }
     }
 
     let largestCurrentFrequency = 0;
     let largestCurrentFrequencyIndex = 0;
 
-    for (i = 0; i < frequency.length; i++) {
-        if (largestCurrentFrequency < frequency[i]) {
-            largestCurrentFrequency = frequency[i];
-            largestCurrentFrequencyIndex = frequency.indexOf(frequency[i]);
+    for (b = 0; b < frequency.length; b++) {
+        if (largestCurrentFrequency < frequency[b]) {
+            largestCurrentFrequency = frequency[b];
+            largestCurrentFrequencyIndex = frequency.indexOf(frequency[b]);
         }
     }
 
     let finalString = "";
 
     let NextMostFrequencyAndIndexFinder = function (databaseInput, excludedIndexs) {
-        let largestCurrentFrequency = -1;
-        let largestCurrentFrequencyIndex = -1;
+        let largestCurrentFrequency = 0;
+        let largestCurrentFrequencyIndex = 0;
 
         for (i = 0; i < databaseInput[1].length; i++) {
-            if ((largestCurrentFrequency < databaseInput[1][i]) && (excludedIndexs.indexOf(databaseInput[1][i]) == -1)) {
+            if ((largestCurrentFrequency <= databaseInput[1][i]) && (excludedIndexs.indexOf(databaseInput[1].indexOf(databaseInput[1][i])) == -1)) {
                 largestCurrentFrequency = databaseInput[1][i];
                 largestCurrentFrequencyIndex = databaseInput[1].indexOf(databaseInput[1][i]);
             }
         }
+
         return [largestCurrentFrequency, largestCurrentFrequencyIndex];
 
     }
@@ -52,13 +53,14 @@ var frequencySort = function (s) {
     let excludedIndexsArray = new Array(frequency.length);
 
     for (a = 0; a < arrayOfString.length; a++) {
-        for (i = largestCurrentFrequency; i > 0; i--) {
+        for (d = largestCurrentFrequency; d > 0; d--) {
             finalString = finalString + characters[largestCurrentFrequencyIndex];
         }
         excludedIndexsArray[a] = largestCurrentFrequencyIndex;
-        console.log(excludedIndexsArray)
+        frequency[largestCurrentFrequencyIndex] = null;
+
         let resultArray = NextMostFrequencyAndIndexFinder(database, excludedIndexsArray);
-        console.log(resultArray)
+
         largestCurrentFrequencyIndex = resultArray[1]
         largestCurrentFrequency = resultArray[0]
     }
